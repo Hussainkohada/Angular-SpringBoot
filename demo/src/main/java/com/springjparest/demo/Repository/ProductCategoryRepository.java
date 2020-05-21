cde3c5f2ed574f1e1d56dbbf88fa55784d96482a
Hussainkohada <46986636+Hussainkohada@users.noreply.github.com>
2020-05-21 14:17:04 +0530

    Add files via upload

--- demo/src/main/java/com/springjparest/demo/Repository/ProductCategoryRepository.java
+++ demo/src/main/java/com/springjparest/demo/Repository/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.springjparest.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.springjparest.demo.Model.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productcategory",path="productcategory")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
 
--- demo/src/main/java/com/springjparest/demo/Repository/ProductRepository.java
+++ demo/src/main/java/com/springjparest/demo/Repository/ProductRepository.java
@@ -2,9 +2,12 @@ package com.springjparest.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.springjparest.demo.Model.Product;
 
+
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "product",path="product")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
